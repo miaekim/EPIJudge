cc58a35686d245c0552bd406ec4da9f4e25db8b1
miaekim <3154473@gmail.com>
2020-10-07 20:50:42 -0700

    is tree symmetric

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -1,10 +1,40 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+import queue
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    if tree == None:
+        return True
+
+    left_q = queue.Queue()
+    if tree.left != None:
+        left_q.put(tree.left)
+    right_q = queue.Queue()
+    if tree.right != None:
+        right_q.put(tree.right)
+
+    while left_q.empty() == False and right_q.empty() == False:
+        l = left_q.get()
+        r = right_q.get()
+
+        if l.data != r.data:
+            return False
+
+        if l.left != None:
+            left_q.put(l.left)
+        if l.right != None:
+            left_q.put(l.right)
+
+        if r.right != None:
+            right_q.put(r.right)
+        if r.left != None:
+            right_q.put(r.left)
+
+    if left_q.empty() == True and right_q.empty() == True:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
