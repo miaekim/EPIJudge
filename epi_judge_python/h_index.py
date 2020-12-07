00bb3e21e7331fee485e26781eaee9971764bbed
miaekim <3154473@gmail.com>
2020-12-07 00:26:07 -0800

    h_index

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def h_index(citations: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    if citations is None:
+        return 0
+
+    n = len(citations)
+
+    if n == 0:
+        return 0
+        
+    arr = sorted(citations)
+
+    h = min(arr[-1], n)
+    while h >= 1:
+        res = arr[n - h]
+        # print("h: ", h ,"res: ", res, "\n")
+        if res >= h:
+            return h
+        else:
+            h = h -1 
+
+        
+    return h
 
 
 if __name__ == '__main__':
