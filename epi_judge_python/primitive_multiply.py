f57c9146d271ca9bd321aeabd51a62d33d1914a5
miaekim <3154473@gmail.com>
2021-06-02 08:50:43 -0700

    primitive multiiply

--- epi_judge_python/primitive_multiply.py
+++ epi_judge_python/primitive_multiply.py
@@ -1,9 +1,34 @@
 from test_framework import generic_test
 
+def addition(x, y):
+    result = 0
+    carry_over = 0
+
+    while True:
+        if x == 0:
+            return y
+        elif y == 0:
+            return x
+        
+        result = x ^ y
+        carry_over = (x & y) << 1
+
+        if carry_over == 0:
+            break
+        else:
+            x = result
+            y = carry_over
+
+    return result 
 
 def multiply(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    while y:
+        if y & 1:
+            result = addition(result, x)
+        y = y >> 1
+        x = x << 1
+    return result
 
 
 if __name__ == '__main__':
