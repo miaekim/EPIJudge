50ce5f660f0f41925831a60fa99cd41dc9eab198
miaekim <3154473@gmail.com>
2021-06-01 22:42:35 -0700

    reverse bits

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,9 +1,14 @@
 from test_framework import generic_test
 
-
 def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    y = 0 
+    i = 0
+    while i < 64:
+        y = y << 1 
+        y |= x & 1
+        x = x >> 1
+        i = i + 1
+    return y
 
 
 if __name__ == '__main__':
