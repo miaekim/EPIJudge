239ad29bee1c2b4aba0a301feaaba624bc59847b
miaekim <3154473@gmail.com>
2021-06-01 22:14:24 -0700

    swap bits

--- epi_judge_python/swap_bits.py
+++ epi_judge_python/swap_bits.py
@@ -2,9 +2,18 @@ from test_framework import generic_test
 
 
 def swap_bits(x, i, j):
-    # TODO - you fill in here.
-    return 0
+    ii = (x >> i) & 1
+    jj = (x >> j) & 1
 
+    if ii ^ jj:
+        if ii == 1:
+            x = x | 2**j
+            x = x & ~2**i
+        else:
+            x = x | 2**i
+            x = x & ~2**j
+
+    return x
 
 if __name__ == '__main__':
     exit(
