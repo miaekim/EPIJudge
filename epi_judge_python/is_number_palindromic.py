4a9fa83a3083473cb09c7f47c33d8dca63114f55
miaekim <3154473@gmail.com>
2021-06-02 20:32:36 -0700

    is_palindrome no str

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,11 +1,16 @@
 from test_framework import generic_test
 
+def reverse_int(x: int) -> int:
+    result = 0
+    while x:
+        result = result * 10 + x % 10
+        x //= 10
+    return result
 
 def is_palindrome_number(x: int) -> bool:
     if x < 0:
         return False
-    return str(x) == str(x)[::-1]
-
+    return x == reverse_int(x)
 
 if __name__ == '__main__':
     exit(
