fd24066507f2adf22b5904cf15ac20faa2d5446d
miaekim <3154473@gmail.com>
2021-06-05 21:41:46 -0700

    advance by offset -1

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -1,12 +1,21 @@
+from epi_judge_python.test_framework.test_failure import TestFailure
 from typing import List
 
 from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+    target = len(A) - 1
+    furthest = 0
+    idx = 0
+    while furthest < target and idx <= furthest:
+        furthest = max(furthest, idx + A[idx])
+        idx += 1
 
+    if furthest >= target:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     exit(
