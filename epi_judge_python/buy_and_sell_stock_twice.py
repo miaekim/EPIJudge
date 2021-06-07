0134a856b89be04468ce47964a3da51d992586c3
miaekim <3154473@gmail.com>
2021-06-07 00:01:42 -0700

    [Solution] buy sell stock twice

--- epi_judge_python/buy_and_sell_stock_twice.py
+++ epi_judge_python/buy_and_sell_stock_twice.py
@@ -2,17 +2,29 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def buy_and_sell_stock_twice(prices: List[float]) -> float:
-    buy_price = prev_price = prices[0]
+
+    buy_price = float('inf')
     profit = 0
-    for price in prices[1:]:
-        if price > prev_price:
-            prev_price = price
-            profit = max(profit, price - buy_price)
-        elif price < buy_price:
-            buy_price = prev_price = price
-    return profit
+    profits = []
+    for price in prices:
+        buy_price = min(buy_price, price)
+        profit = max(profit, price - buy_price)
+        profits.append(profit)
+
+    backward_profits = []
+    sell_price = 0
+    profit = 0
+    for price in reversed(prices):
+        sell_price = max(sell_price, price)
+        profit = max(profit, sell_price - price)
+        backward_profits.append(profit)
+
+    total_profit = 0
+    for p1, p2 in zip(profits, reversed(backward_profits)):
+        total_profit = max(total_profit, p1 + p2)
+    return total_profit
+
 
 
 if __name__ == '__main__':
