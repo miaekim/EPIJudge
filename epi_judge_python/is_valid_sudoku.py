6e0d735bff7bda64742e49e952b693aa4c314280
miaekim <3154473@gmail.com>
2021-06-12 15:20:06 -0700

    5.17 Validate sudoku

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -5,7 +5,38 @@ from test_framework import generic_test
 
 # Check if a partially filled matrix has any conflicts.
 def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
-    # TODO - you fill in here.
+    sudoku_size = 9
+    sub_size = 3
+    # check if there are dupe numbers in row
+    for i in range(sudoku_size):
+        visited = [False] * sudoku_size
+        for j in range(sudoku_size):
+            if partial_assignment[i][j] != 0:
+                if visited[partial_assignment[i][j]-1] == False:
+                    visited[partial_assignment[i][j]-1] = True
+                else:
+                    return False
+    
+    for j in range(sudoku_size):
+        visited = [False] * sudoku_size
+        for i in range(sudoku_size):
+            if partial_assignment[i][j] != 0:
+                if visited[partial_assignment[i][j]-1] == False:
+                    visited[partial_assignment[i][j]-1] = True
+                else:
+                    return False
+
+    for i in range(0,sudoku_size,3):
+        for j in range(0,sudoku_size,3):
+            visited = [False] * sudoku_size
+            for _i in range(0,sub_size):
+                for _j in range(0,sub_size):
+                    if partial_assignment[i+_i][j+_j] != 0:
+                        if visited[partial_assignment[i+_i][j+_j] -1] == False:
+                            visited[partial_assignment[i+_i][j+_j] - 1] = True
+                        else:
+                            return False
+    
     return True
 
 
