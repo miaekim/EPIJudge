91f2babff8f840cd7669441ad80fdf365e70f096
miaekim <3154473@gmail.com>
2021-06-13 09:27:35 -0700

    6.4 replace and remove brute force, O(n^2)

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -4,10 +4,25 @@ from typing import List
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
-
+import functools
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    final_size = size
+    idx = 0
+    while idx < final_size:
+        if s[idx] == 'b':
+            for i in range(idx + 1, final_size):
+                s[i - 1] = s[i]
+            final_size -= 1
+        if s[idx] == 'a':
+            for i in reversed(range(idx + 1, final_size)):
+                s[i + 1] = s[i]
+            s[idx] = 'd'
+            s[idx + 1] = 'd'
+            final_size += 1
+            idx += 2
+        if idx < final_size and s[idx] != 'a' and s[idx] != 'b':
+            idx += 1 
+    return final_size
 
 
 @enable_executor_hook
