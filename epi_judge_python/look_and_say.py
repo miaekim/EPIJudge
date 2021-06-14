3423af1d9139ec7bb4e1fd31c7102d956005b6ad
miaekim <3154473@gmail.com>
2021-06-13 17:40:59 -0700

    6.7 look and say O(n* length_of_array_foeach_step)

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -1,9 +1,44 @@
 from test_framework import generic_test
 
-
+from typing import List
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    
+    def say(prev: List[str]):
+        n = len(prev)
+        count = 1
+        counting = prev[0]
+        write_idx = 0
+        for num_str in prev[1:]:
+            if num_str == counting:
+                count += 1
+            else:
+                if write_idx < n:
+                    prev[write_idx] = str(count)
+                else:
+                    prev.append(str(count))
+
+                if write_idx + 1 < n:
+                    prev[write_idx + 1] = counting
+                else:
+                    prev.append(counting)
+                count = 1
+                counting = num_str
+                write_idx += 2
+        
+        if write_idx < n:
+            prev[write_idx] = str(count)
+        else:
+            prev.append(str(count))
+
+        if write_idx + 1 < n:
+            prev[write_idx + 1] = counting
+        else:
+            prev.append(counting)
+
+    prev = ['1']
+    for x in range(n - 1):
+        say(prev)
+    return "".join(prev)
 
 
 if __name__ == '__main__':
