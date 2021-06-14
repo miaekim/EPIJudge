7efe8f28d16b199addfb9311811de47194b1f0b1
miaekim <3154473@gmail.com>
2021-06-13 23:00:49 -0700

    6.12 substring match brute force O(n*m)

--- epi_judge_python/substring_match.py
+++ epi_judge_python/substring_match.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def rabin_karp(t: str, s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    n, m =  len(t), len(s)
+    idx = 0
+
+    while idx < n:
+        i = 0
+        while idx + i < n and i < m and t[idx + i] == s[i]:
+            i += 1
+        if i == m:
+            return idx 
+        idx += 1 
+    return -1
 
 
 if __name__ == '__main__':
