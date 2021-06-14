602fc2e76b8d2312c5138c19c78c22dd644061a9
miaekim <3154473@gmail.com>
2021-06-13 19:23:12 -0700

    6.9 is valid ip address O(1)

--- epi_judge_python/valid_ip_addresses.py
+++ epi_judge_python/valid_ip_addresses.py
@@ -2,10 +2,30 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def get_valid_ip_address(s: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    def is_valid_part(s: str):
+        return len(s) == 1 or (s[0] != '0' and int(s) < 256)
+        
+    def get_partial_ip_address(prefix: str, s: str, dot_count: int):
+        if dot_count == 1:
+            if is_valid_part(s):
+                return [f"{prefix}.{s}"]
+            else:
+                return []
+
+        ip_addresses = []
+        n = len(s)
+        dot_idx = 1
+        while dot_idx < n:
+            if is_valid_part(s[:dot_idx]):
+                new_prefix = f"{prefix}.{s[:dot_idx]}" if prefix != "" else f"{s[:dot_idx]}"
+                ip_addresses += get_partial_ip_address(new_prefix, s[dot_idx:], dot_count - 1)
+                dot_idx += 1
+            else:
+                break
+        return ip_addresses
+
+    return get_partial_ip_address("", s, 4)
 
 
 def comp(a, b):
