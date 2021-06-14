bff39a655c256575a4effdb8faae92e15a4bfdeb
miaekim <3154473@gmail.com>
2021-06-13 18:33:38 -0700

    6.8 convert roma to int, time O(n), space O(1)

--- epi_judge_python/roman_to_integer.py
+++ epi_judge_python/roman_to_integer.py
@@ -1,9 +1,18 @@
 from test_framework import generic_test
 
-
+T = {'': 0, 'I': 1, 'V':5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+E = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
 def roman_to_integer(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    prev = s[0]
+    result = T[prev]
+    for _s in s[1:]:
+        if prev + _s in E:
+            result += (E[prev+_s] - T[prev])
+            prev = ''
+        else:
+            prev = _s 
+            result += T[prev]
+    return result
 
 
 if __name__ == '__main__':
