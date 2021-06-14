6d7a6f8ebe094bdf98652ec40f589a81fb27bef7
miaekim <3154473@gmail.com>
2021-06-13 19:32:32 -0700

    6.10 snake string O(n)

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    n = len(s)
+    snake = ''
+    if n <= 0:
+        return snake
+    for idx in range(1,n,4):
+        snake += s[idx]
+    for idx in range(0,n, 2):
+        snake += s[idx]
+    if n <= 3:
+        return s[1]+s[0]+s[2:n]
+    for idx in range(3,n, 4):
+        snake += s[idx]
+    return snake
 
 
 if __name__ == '__main__':
