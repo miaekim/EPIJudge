39dce6c5a94d2c05f2cc753a9ebdae6858b682e8
miaekim <3154473@gmail.com>
2021-06-20 10:57:38 -0700

    8.4 normalize path time O(n).. think for edge case, when should the operation happen

--- epi_judge_python/directory_path_normalization.py
+++ epi_judge_python/directory_path_normalization.py
@@ -1,9 +1,28 @@
+from os import PathLike
 from test_framework import generic_test
 
 
 def shortest_equivalent_path(path: str) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not path:
+        return path
+    # ./ ignore
+    # ../ remove current folder
+    if path[0] == "/":
+        path = path[1:]
+        root = "/"
+    else:
+        root = ""
+
+    path_stack = []
+    for _path in path.split("/"):
+        if _path == "." or _path == "":
+            pass
+        elif _path == ".." and path_stack and path_stack[-1] != "..":
+            path_stack.pop()
+        else:
+            path_stack.append(_path)
+        
+    return root + "/".join(path_stack)
 
 
 if __name__ == '__main__':
