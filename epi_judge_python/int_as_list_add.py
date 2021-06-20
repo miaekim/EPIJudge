e37691ae1c4df471fa407a87226635c18cfd9c9a
miaekim <3154473@gmail.com>
2021-06-19 21:34:05 -0700

    7.13 add two num time O(n+m)  memory O(max(n,m))

--- epi_judge_python/int_as_list_add.py
+++ epi_judge_python/int_as_list_add.py
@@ -5,8 +5,30 @@ from test_framework import generic_test
 
 
 def add_two_numbers(L1: ListNode, L2: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    result_head = ListNode()
+    result = result_head
+    carry = 0
+    while L1 or L2:
+        if L1:
+            l1 = L1.data
+            L1 = L1.next
+        else:
+            l1 = 0
+
+        if L2:
+            l2 = L2.data
+            L2 = L2.next
+        else:
+            l2 = 0
+
+        _sum = l1 + l2 + carry
+        carry = _sum // 10
+        left = _sum % 10
+        result.next = ListNode(left)
+        result = result.next
+    if carry:
+        result.next = ListNode(carry)
+    return result_head.next
 
 
 if __name__ == '__main__':
