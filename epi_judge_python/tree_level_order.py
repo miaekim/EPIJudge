4a8462178a334f9b1b9009c1684c67290a4caf8a
miaekim <3154473@gmail.com>
2021-06-20 22:33:00 -0700

    8.6 BFS O(n)

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -3,10 +3,29 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from collections import deque
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def get_data(depth: List[BinaryTreeNode]) -> List[int]:
+        return [d.data for d in depth]
+
+    if not tree:
+        return []
+    bfs = []
+    depth = deque([tree])
+    while depth:
+        bfs.append(get_data(depth))
+        next_ = deque([])
+        while depth:
+            next_node = depth.popleft()
+            left = next_node.left
+            right = next_node.right
+            if left:
+                next_.append(left)
+            if right:
+                next_.append(right)
+        depth = next_
+    return bfs
 
 
 if __name__ == '__main__':
