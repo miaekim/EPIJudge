c0d507b7447f14e17f23a93f6b00776cf98b1ba8
miaekim <3154473@gmail.com>
2021-06-23 22:25:25 -0700

    9.5 sum root to leaf, time O(n), space O(h)

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -1,10 +1,21 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def traverse(tree: BinaryTreeNode, _sum: int):
+    if tree:
+        _sum = (_sum << 1) + tree.data
+        if tree.left and tree.right:
+            _sum = traverse(tree.left, _sum) + traverse(tree.right, _sum) 
+        elif tree.left:
+            _sum = traverse(tree.left, _sum)
+        elif tree.right:
+            _sum = traverse(tree.right, _sum)
+    return _sum
+
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
     # TODO - you fill in here.
-    return 0
+    return traverse(tree, 0)
 
 
 if __name__ == '__main__':
