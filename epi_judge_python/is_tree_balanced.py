0c7a78186f4366a6838f759328044284ac22ae28
miaekim <3154473@gmail.com>
2021-06-23 19:10:43 -0700

    9.1 is balanced time O(n)

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,10 +1,23 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from collections import deque
+
+def traverse(tree: BinaryTreeNode, depth: int):
+    if not tree:
+        return depth, True
+    else:
+        depth_left, is_left_balanced = traverse(tree.left, depth + 1)
+        depth_right, is_right_balanced = traverse(tree.right, depth + 1)
+        if is_left_balanced and is_right_balanced:
+            return (max(depth_left, depth_right), abs(depth_left - depth_right) <= 1)
+        else:
+            return (max(depth_left, depth_right), False)
+
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    _, is_balanced = traverse(tree, 0)
+    return is_balanced
 
 
 if __name__ == '__main__':
