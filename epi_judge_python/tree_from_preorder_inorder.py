d273dbacacefe623cd129c66e002f30005bd4c86
miaekim <3154473@gmail.com>
2021-06-27 00:50:11 -0700

    9.11 with help reconstruct, time O(n), space O(n)

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -6,8 +6,14 @@ from test_framework import generic_test
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    if not preorder:
+        return None
+    root = BinaryTreeNode(preorder[0])
+    if len(preorder) >  1:
+        idx = inorder.index(root.data)
+        root.left = binary_tree_from_preorder_inorder(preorder[1:1+idx],inorder[:idx])
+        root.right = binary_tree_from_preorder_inorder(preorder[1+idx:],inorder[idx+1:])
+    return root
 
 
 if __name__ == '__main__':
