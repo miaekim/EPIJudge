af27e99ba3235e10496b96659002c975d38b532d
miaekim <3154473@gmail.com>
2021-06-27 20:57:54 -0700

    6.1 time O(nlogk), k is size of merged buffer

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -4,11 +4,24 @@ from test_framework import generic_test
 import heapq
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    merged = [x for array in sorted_arrays for x in array]
+    merged = []
     heapq.heapify(merged)
     result = []
+    columns = [0] * len(sorted_arrays)
+    columns_max = [len(arr) for arr in sorted_arrays]
+
+    for idx, arr in enumerate(sorted_arrays):
+        if columns_max[idx] > 0:
+            heapq.heappush(merged, (arr[columns[idx]], idx))
+            columns[idx] += 1
+
     while merged:
-        result.append(heapq.heappop(merged))
+        _data, _idx = heapq.heappop(merged)
+        result.append(_data)
+        if columns[_idx] < columns_max[_idx]:
+            heapq.heappush(merged, (sorted_arrays[_idx][columns[_idx]], _idx))
+            columns[_idx] += 1
+
     return result
     
     
