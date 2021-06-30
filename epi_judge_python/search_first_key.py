d9438aaa9ae30469490ae80acfddc30d436a9866
miaekim <3154473@gmail.com>
2021-06-30 09:26:05 -0700

    11.1 search first key time without library O(logn), space O(1)

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,13 +2,20 @@ from typing import List
 
 from test_framework import generic_test
 
-import bisect
 def search_first_of_k(A: List[int], k: int) -> int:
-    result = bisect.bisect_left(A, k)
-    if not A or result == len(A) or A[result] != k:
-        return -1
-    else:
-        return result
+    if not A or k < A[0]:
+        return - 1
+
+    L, U = 0, len(A) - 1
+    while L <= U:
+        M = (L + U) // 2
+        if A[M] == k and (M == 0 or A[M - 1] < k):
+            return M
+        elif A[M] < k:
+            L = M + 1
+        else:
+            U = M - 1
+    return -1        
 
 
 if __name__ == '__main__':
