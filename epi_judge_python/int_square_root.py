733f04429a3c676313a0fc7443499ffdabc9778c
miaekim <3154473@gmail.com>
2021-07-02 09:18:34 -0700

    11.4 sqrt, time O(logk) space O(1)

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    digit = len(str(k))
+    if k == 0:
+        return 0
+
+    left, right = 10 ** ((digit - 1) // 2) , 10 ** ((digit + 1) // 2)
+    while left < right:
+        mid = (left + right) // 2
+        if mid ** 2 <= k:
+            if (mid + 1) ** 2 > k:
+                return mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right
 
 
 if __name__ == '__main__':
