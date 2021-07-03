02e637fcb72d459cdec74820057e20d48b6b6b65
miaekim <3154473@gmail.com>
2021-07-03 08:59:31 -0700

    11.5 sqrt time O(logx/epsilion) space O(1)

--- epi_judge_python/real_square_root.py
+++ epi_judge_python/real_square_root.py
@@ -1,9 +1,29 @@
 from test_framework import generic_test
+import sys
 
-
+EIPSLION = sys.float_info.epsilon * 10
 def square_root(x: float) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    reverse = False
+    if x < 1:
+        reverse = True
+        x = 1/x
+    left, right = 0, x
+    while left<right:
+        mid = (left + right) / 2
+        if mid ** 2 < x:
+            if mid == left:
+                return 1/mid if reverse else mid
+            left = mid + EIPSLION
+        elif mid ** 2 > x:
+            if mid == right:
+                return 1/mid if reverse else mid
+            right = mid - EIPSLION
+        else:
+            if reverse:
+                return 1/mid
+            return mid
+
+    return 1/left if reverse else left
 
 
 if __name__ == '__main__':
