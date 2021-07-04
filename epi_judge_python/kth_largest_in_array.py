fc31d60dead71bfe52b55a71468e716c489aa943
miaekim <3154473@gmail.com>
2021-07-03 18:46:25 -0700

    11.8 kth largest elem, time O(nlogk) space O(k)

--- epi_judge_python/kth_largest_in_array.py
+++ epi_judge_python/kth_largest_in_array.py
@@ -2,13 +2,25 @@ from typing import List
 
 from test_framework import generic_test
 
+import heapq
 
 # The numbering starts from one, i.e., if A = [3, 1, -1, 2]
 # find_kth_largest(1, A) returns 3, find_kth_largest(2, A) returns 2,
 # find_kth_largest(3, A) returns 1, and find_kth_largest(4, A) returns -1.
 def find_kth_largest(k: int, A: List[int]) -> int:
     # TODO - you fill in here.
-    return 0
+    min_heap_size,  min_heap =  0, []
+    heapq.heapify(min_heap)
+
+    for a in A:
+        if min_heap_size == k:
+            if a > min_heap[0]:
+                heapq.heappushpop(min_heap, a)
+        else:
+            heapq.heappush(min_heap, a)
+            min_heap_size += 1
+    
+    return min_heap[0]
 
 
 if __name__ == '__main__':
