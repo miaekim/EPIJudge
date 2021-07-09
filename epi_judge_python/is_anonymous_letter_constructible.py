5e4672826fa97865def60f3f4d11dacb496dc1ad
miaekim <3154473@gmail.com>
2021-07-08 21:07:03 -0700

    12.2 time O(m + n) space O(m + n)

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,17 @@
+import collections
 from test_framework import generic_test
 
-
+from collections import Counter
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
+    magazine_dic = collections.Counter(magazine_text)
+    letter_dic = collections.Counter(letter_text)
+    
+    for k, v in letter_dic.items():
+        if k not in magazine_dic:
+            return False
+        if magazine_dic[k] < v:
+            return False
     return True
 
 
