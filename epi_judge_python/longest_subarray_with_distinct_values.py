6acb8c231dcce128cce1f2d76a6f462615f81323
miaekim <3154473@gmail.com>
2021-07-10 22:01:19 -0700

    12.8 find longest subarray with distinct entries time O(n*m) where m is size of maximum sublist, space O(n)

--- epi_judge_python/longest_subarray_with_distinct_values.py
+++ epi_judge_python/longest_subarray_with_distinct_values.py
@@ -2,10 +2,27 @@ from typing import List
 
 from test_framework import generic_test
 
+from collections import Counter
+"""
 
+[1, 2, 1, 3, 1, 2, 1]	3	TODO
+"""
 def longest_subarray_with_distinct_entries(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    subarray = []
+    _max = float('-inf')
+    for idx, a in enumerate(A):
+        if a in subarray:
+            n = len(subarray)
+            if n > _max:
+                _max = n
+            dupe_idx = subarray.index(a)
+            subarray = subarray[dupe_idx+1:] if dupe_idx < n - 1 else []
+        subarray.append(a)
+
+    n = len(subarray)
+    if n > _max:
+        _max = n
+    return _max
 
 
 if __name__ == '__main__':
