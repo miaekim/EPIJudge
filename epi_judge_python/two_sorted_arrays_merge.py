e89575547198d1ebaee31700615731e4c0f3bb26
miaekim <3154473@gmail.com>
2021-07-18 00:58:05 -0700

    13.2 two sorted arrays merge, time O(m+n) space O(1)

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,7 +5,25 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    if not B:
+        return A
+
+    idx_A, idx_B = m -1, n - 1
+    idx = idx_A + idx_B + 1
+    while idx_A >= 0 and idx_B >= 0:
+        idx = idx_A + idx_B + 1
+        if A[idx_A] > B[idx_B]:
+            A[idx] = A[idx_A]
+            idx_A -= 1
+        else:
+            A[idx] = B[idx_B]
+            idx_B -= 1
+    
+    while idx_B >= 0:
+        idx = idx_A + idx_B + 1
+        A[idx] = B[idx_B]
+        idx_B -= 1
+
     return
 
 
