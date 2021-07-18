54af14732ebeba594be6ea6a55d55dda74b1d55f
miaekim <3154473@gmail.com>
2021-07-18 16:37:57 -0700

    13.6 time O(max(nlogn, max_val)) space O(n)

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -10,8 +10,22 @@ Event = collections.namedtuple('Event', ('start', 'finish'))
 
 
 def find_max_simultaneous_events(A: List[Event]) -> int:
-    # TODO - you fill in here.
-    return 0
+    starts = sorted([a.start for a in A])
+    finishs = sorted([a.finish for a in A])
+
+    n = len(A)
+    start_idx, finish_idx = 0, 0
+    simultaneous = 0
+    max_simultaneous = 0
+    for x in range(0, finishs[-1]+1):
+        while start_idx < n and starts[start_idx] <= x:
+            start_idx += 1        
+            simultaneous += 1
+        max_simultaneous = max(max_simultaneous, simultaneous)
+        while finish_idx < n and  finishs[finish_idx] <= x:
+            finish_idx += 1
+            simultaneous -= 1
+    return max_simultaneous
 
 
 @enable_executor_hook
