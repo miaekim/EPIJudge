c45db885f5eb31f62f99dcfb8d555ab15741e393
miaekim <3154473@gmail.com>
2021-07-24 22:45:05 -0700

    13.12 time O(n log n) space O1

--- epi_judge_python/find_salary_threshold.py
+++ epi_judge_python/find_salary_threshold.py
@@ -5,9 +5,6 @@ from test_framework import generic_test
 
 
 def find_salary_cap(target_payroll: int, current_salaries: List[int]) -> float:
-    if target_payroll == sum(current_salaries):
-        return max(current_salaries)
-
     n = len(current_salaries)
     current_salaries.sort()
 
