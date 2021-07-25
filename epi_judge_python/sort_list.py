852a2503ea00876a9c3c572ad67795a78f4a8a80
miaekim <3154473@gmail.com>
2021-07-24 20:56:10 -0700

    13.11 inserstion sort O(n^2), space O(n)

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -5,8 +5,27 @@ from test_framework import generic_test
 
 
 def stable_sort_list(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L is None:
+        return None
+    indexes = []
+    index = 0
+    while L:
+        indexes.append(L)
+
+        j = index - 1
+        while j >= 0 and L.data < indexes[j].data:
+            indexes[j + 1] = indexes[j]
+            j -= 1
+        indexes[j + 1] = L
+
+        index += 1
+        L = L.next
+
+    for idx in range(index - 1):
+        indexes[idx].next = indexes[idx + 1]
+    indexes[-1].next = None
+
+    return indexes[0]
 
 
 if __name__ == '__main__':
