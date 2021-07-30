19afb28c15dc0c1fd84df433a0190ea4d0fb8c01
miaekim <3154473@gmail.com>
2021-07-30 14:23:57 -0700

    is bst or not: time O(n), space O(1)

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,11 +1,32 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+def is_binary_tree_bst_with_bound(tree: BinaryTreeNode, lower_bound, upper_bound) -> bool:
+    if not tree:
+        return True
+
+    left = is_binary_tree_bst_with_bound(tree.left, lower_bound, tree.data)
+    if not left or (tree.left and tree.left.data < lower_bound):
+        return False
+    right = is_binary_tree_bst_with_bound(tree.right, tree.data, upper_bound)
+    if not right or (tree.right and tree.right.data > upper_bound):
+        return False
+
+    okay_left = False
+    if not tree.right or (tree.right and tree.data <= tree.right.data):
+        okay_left = True
+
+    okay_right = False
+    if not tree.left or (tree.left and tree.data >= tree.left.data):
+        okay_right = True
+
+    return okay_left and okay_right
+    
+        
+    
 
 def is_binary_tree_bst(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
-
+    return is_binary_tree_bst_with_bound(tree, float('-inf'), float('inf'))
 
 if __name__ == '__main__':
     exit(
