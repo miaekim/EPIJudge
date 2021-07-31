c5b90f49566406e1ca8dcc9ad285696f47832151
miaekim <3154473@gmail.com>
2021-07-31 10:34:45 -0700

    14.6 find closest elements Time O(n^2) Space O(n)

--- epi_judge_python/minimum_distance_3_sorted_arrays.py
+++ epi_judge_python/minimum_distance_3_sorted_arrays.py
@@ -5,8 +5,19 @@ from test_framework import generic_test
 
 def find_closest_elements_in_sorted_arrays(sorted_arrays: List[List[int]]
                                            ) -> int:
-    # TODO - you fill in here.
-    return 0
+    sorted_iters = [iter(array) for array in sorted_arrays]
+    sub_arr = [(next(array), idx) for idx, array in enumerate(sorted_iters)]
+    min_range = range = max(sub_arr)[0] - min(sub_arr)[0]
+    while all(sub_arr):
+        _, min_idx = min(sub_arr)
+        sub_arr[min_idx] = (next(sorted_iters[min_idx], None), min_idx)
+        if sub_arr[min_idx][0] == None:
+            break
+        range = max(sub_arr)[0] - min(sub_arr)[0]
+
+        if range < min_range:
+            min_range = range
+    return min_range
 
 
 if __name__ == '__main__':
