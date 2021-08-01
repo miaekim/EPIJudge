94f8bbef727d9e72cfe2b27e651ec882105deae2
miaekim <3154473@gmail.com>
2021-07-31 17:35:08 -0700

    14.10 range lookup, m: data in range count, time O(m log m), space O(m)

--- epi_judge_python/range_lookup_in_bst.py
+++ epi_judge_python/range_lookup_in_bst.py
@@ -4,12 +4,28 @@ from typing import List
 from bst_node import BstNode
 from test_framework import generic_test
 
+from collections import deque
+
 Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
 def range_lookup_in_bst(tree: BstNode, interval: Interval) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    left, right = interval.left, interval.right
+
+    to_visit = deque([tree])
+    result = []
+    while to_visit:
+        node = to_visit.popleft()
+        if not node:
+            continue
+
+        if node.data >= left:
+            to_visit.append(node.left)
+        if node.data <= right:
+            to_visit.append(node.right)
+        if left <= node.data <= right:
+            result.append(node.data)
+    return sorted(result)
 
 
 def range_lookup_in_bst_wrapper(tree, i):
