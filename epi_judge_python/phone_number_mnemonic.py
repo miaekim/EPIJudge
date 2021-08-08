87d8b5158f54cf7bcd7672a3947b1bbb45c991d0
miaekim <3154473@gmail.com>
2021-08-08 13:06:40 -0700

    15.2 phone number Time O(n4^n) space O(4^n)

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -4,8 +4,30 @@ from test_framework import generic_test, test_utils
 
 
 def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+    number_to_chars = {
+        '0': ['0'],
+        '1': ['1'],
+        '2': ['A','B','C'],
+        '3': ['D','E','F'],
+        '4': ['G','H','I'],
+        '5': ['J','K','L'],
+        '6': ['M','N','O'],
+        '7': ['P','Q','R', 'S'],
+        '8': ['T','U','V'],
+        '9': ['W','X','Y','Z']
+    }
+    temps = []
+    results: List[str] = [""]
+
+    for number in phone_number: # n
+        chars = number_to_chars[number]
+        for result in results: # n
+            for char in chars: # 4
+                temps.append(result + char)
+        results = temps
+        temps =[]
+
+    return results
 
 
 if __name__ == '__main__':
