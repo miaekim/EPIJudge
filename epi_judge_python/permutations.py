084d4fd101039bb7346eb862fa81d4cc8d4ce6ba
miaekim <3154473@gmail.com>
2021-08-08 18:06:12 -0700

    15.04 permutation, O(n!)

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -3,9 +3,20 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
-def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+def permutations(nums: List[int]) -> List[List[int]]:
+    def perm(nums: List[int], n: int) -> List[List[int]]:
+        if n == 1:
+            return [nums]
+        
+        result = []
+        nums_set = set(nums)
+        for num in nums:
+            small_nums = nums_set - set([num])
+            small_perms = perm(list(small_nums), n -1)
+            for small_perm in small_perms: 
+                result.append([num] + small_perm)
+        return result
+    return perm(nums, len(nums))
 
 
 if __name__ == '__main__':
