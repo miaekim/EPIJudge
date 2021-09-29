bf79e286f01429bb9b8ae8a1d7f3b5cc7b4cbfb3
miaekim <3154473@gmail.com>
2021-09-28 20:07:10 -0700

    16.4 binomial coff O(n) memory O(n)

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -2,8 +2,10 @@ from test_framework import generic_test
 
 
 def compute_binomial_coefficient(n: int, k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    factorials = [1, 1]
+    for num in range(2, n + 1):
+        factorials.append(factorials[-1] * num)
+    return (factorials[n] // factorials[n - k]) // factorials[k]
 
 
 if __name__ == '__main__':
