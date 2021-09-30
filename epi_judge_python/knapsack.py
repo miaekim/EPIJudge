3b960585f3421386d8c0ba8aad390c43ef82c2cf
miaekim <3154473@gmail.com>
2021-09-29 20:55:27 -0700

    16.6 O(item_cnt * capacity)

--- epi_judge_python/knapsack.py
+++ epi_judge_python/knapsack.py
@@ -1,20 +1,35 @@
 import collections
 import functools
 from typing import List
-
+from collections import defaultdict
 from test_framework import generic_test
 from test_framework.test_utils import enable_executor_hook
 
 Item = collections.namedtuple('Item', ('weight', 'value'))
 
-
 def optimum_subject_to_capacity(items: List[Item], capacity: int) -> int:
-    sorted(items, lambda x: x.weight)
-    memory = [[0] * (capacity+1)] * len(items)
-    for idx, item in enumerate(items):
-        for j in range(1, capacity):
-            memory[idx][j] = max(memory[idx][j - 1], memory[idx - item.weight][j] + item.value)
-    return 0
+    n = len(items)
+    memory = {}
+    def knapsack(item_idx: int, cap: int) -> int:
+        nonlocal items
+        nonlocal n
+        if (item_idx, cap) in memory:
+            return memory[(item_idx, cap)]
+        if item_idx < 0 or cap == 0:
+            return 0
+
+        item = items[item_idx]
+
+        if item.weight > cap:
+            memory[(item_idx, cap)] = knapsack(item_idx - 1, cap)
+            return memory[(item_idx, cap)]
+        memory[(item_idx, cap)] = max(
+            item.value + knapsack(item_idx - 1, cap - item.weight),
+            knapsack(item_idx - 1, cap)
+        )
+        return memory[(item_idx, cap)]
+
+    return knapsack(n - 1, capacity)
 
 
 @enable_executor_hook
