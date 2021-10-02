a389ab2d1ea1130dbee423a7e9abf7b4000b9f4b
miaekim <3154473@gmail.com>
2021-10-02 11:41:09 -0700

    16.11 time O(n^2) sapce O(n^2)

--- epi_judge_python/pretty_printing.py
+++ epi_judge_python/pretty_printing.py
@@ -1,11 +1,33 @@
+import functools
 from typing import List
-
 from test_framework import generic_test
 
 
 def minimum_messiness(words: List[str], line_length: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # greedy, last line
+    n = len(words)
+    word_lens = [len(w) for w in words]
+    cost = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(i, n):
+            selected_length = sum(word_lens[i : j + 1]) + (j - i)
+            if selected_length > line_length:
+                cost[i][j] = float("inf")
+            else:
+                cost[i][j] = (line_length - selected_length) ** 2
+
+    @functools.lru_cache(None)
+    def line_messiness(start: int):
+        nonlocal cost
+        nonlocal line_length
+        if start >= n:
+            return 0
+
+        return min([
+           (cost[start][k] + line_messiness(k + 1)) for k in range(start, n)
+        ])
+    return line_messiness(0)
 
 
 if __name__ == '__main__':
