15aee95ea86a24c7ebf2e4f1479c7130b1f1a9e1
miaekim <3154473@gmail.com>
2021-10-03 00:00:14 -0700

    16.10 time O(n * n)

--- epi_judge_python/longest_nondecreasing_subsequence.py
+++ epi_judge_python/longest_nondecreasing_subsequence.py
@@ -1,11 +1,28 @@
+import functools
 from typing import List
-
+import functools
 from test_framework import generic_test
 
 
 def longest_nondecreasing_subsequence_length(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    n = len(A)
+    longest_length = 1
+    @functools.lru_cache(None)
+    def subseq_length_at(end: int) -> int:
+        nonlocal A
+        nonlocal longest_length
+        if end == 0:
+            subseq_length = 1
+        else:
+            subseq_length = max([
+                (subseq_length_at(idx) + 1) if A[end] >= A[idx] else 1 for idx in range(end)
+            ])
+        longest_length = max(longest_length, subseq_length)
+        return subseq_length
+
+    for idx in range(n):
+        subseq_length_at(idx)
+    return longest_length
 
 
 if __name__ == '__main__':
